8b888f2635ae14e96717e1eb9b1fa15369ccc4b5
Максим <maks.greb2@gmail.com>
2020-05-02 14:29:14 +0300

    Стрелять можно только когда подобрал патроны

---
+++ Assets/Scripts/Ammunition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ammunition : MonoBehaviour
+{
+    [SerializeField] private Button _shootButton;
+
+    private void OnMouseDown()
+    {
+        _shootButton.interactable = true;
+        Destroy(gameObject, 0.1f);
+    }
+}
--- Assets/Scripts/SetTarget.cs
+++ Assets/Scripts/SetTarget.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SetTarget : MonoBehaviour
 {
     [SerializeField] private GameObject[] _players;
+    [SerializeField] private Button _shootButton;
+
 
     private void OnMouseDown()
     {
@@ -20,5 +23,6 @@ public class SetTarget : MonoBehaviour
                 }
             }
         }
+        _shootButton.interactable = false;
     }
 }
