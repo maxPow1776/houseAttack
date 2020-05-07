1c0db82e3f8ce15f1e7855ee3621de907359b01a
Максим <maks.greb2@gmail.com>
2020-05-07 14:30:36 +0300

    Добавил появление игрока

---
+++ Assets/Scripts/Multiplayer/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject _playerPrefab;
+    [SerializeField] private GameObject _firstPosition;
+    [SerializeField] private GameObject _secondPosition;
+
+    void Start()
+    {
+        Vector3 currentPosition = Vector3.zero;
+        var firstPosition = _firstPosition.GetComponent<Position>();
+        if(firstPosition != null)
+        {
+            if(firstPosition.IsEmpty)
+            {
+                currentPosition = _firstPosition.transform.position;
+                firstPosition.IsEmpty = false;
+            } else
+            {
+                var secondPosition = _secondPosition.GetComponent<Position>();
+                if(secondPosition != null)
+                {
+                    currentPosition = _secondPosition.transform.position;
+                    secondPosition.IsEmpty = false;
+                }
+            }
+        }
+        PhotonNetwork.Instantiate(_playerPrefab.name, currentPosition, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Exit()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("Player enter " + newPlayer.NickName);
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        Debug.Log("Player left " + otherPlayer.NickName);
+    }
+}
---
+++ Assets/Scripts/Multiplayer/PlayerControllerMultiplayer.cs
@@ -0,0 +1,43 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerControllerMultiplayer : MonoBehaviour
+{
+    [SerializeField] private LayerMask _canClick;
+    private NavMeshAgent _navMeshAgent;
+    //[SerializeField] private GameObject[] _team;
+    //private Vector3[] _distance = { new Vector3(-3, 0, 3), new Vector3(3, 0, 3), new Vector3(0, 0, 6) };
+    private PhotonView _photonView;
+
+    private void Start()
+    {
+        _photonView = GetComponent<PhotonView>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (!_photonView.IsMine) return;
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(ray, out raycastHit, 100, _canClick))
+            {
+                _navMeshAgent.SetDestination(raycastHit.point);
+                //for (int i = 0; i < _team.Length; i++)
+                //{
+                //    if (_team[i] != null)
+                //    {
+                //        var autoMove = _team[i].GetComponent<AutoMove>();
+                //        if (autoMove != null)
+                //            autoMove.FollowMe(new Vector3(raycastHit.point.x + _distance[i].x, raycastHit.point.y, raycastHit.point.z + _distance[i].z));
+                //    }
+                //}
+            }
+        }
+    }
+}
