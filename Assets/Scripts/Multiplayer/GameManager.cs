d86f2ccc2184852d45709a02ee9b94b07952c52f
Максим <maks.greb2@gmail.com>
2020-05-09 13:35:18 +0300

    Сделал нормальное появление игроков

--- Assets/Scripts/Multiplayer/GameManager.cs
+++ Assets/Scripts/Multiplayer/GameManager.cs
@@ -5,35 +5,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviourPunCallbacks, IPunObservable
+public class GameManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] private GameObject _playerPrefab;
     [SerializeField] private GameObject _firstPosition;
     [SerializeField] private GameObject _secondPosition;
-    private PhotonView _photonView;
-    private bool _isFirst = true;
-    private int count = 0;
 
     void Start()
     {
-        _photonView = GetComponent<PhotonView>();
-        if (_isFirst)
+        if (PhotonNetwork.CurrentRoom.PlayerCount > 1)
         {
-            PhotonNetwork.Instantiate(_playerPrefab.name, _firstPosition.transform.position, Quaternion.identity);
-            Debug.Log("Instantiate " + count);
-            count++;
-            Debug.Log("Instantiate " + count);
-            if (_photonView.IsMine)
-                _isFirst = false;
+            PhotonNetwork.Instantiate(_playerPrefab.name, _secondPosition.transform.position, new Quaternion(0f, 180f, 0f, 0f));
         }
         else
-            PhotonNetwork.Instantiate(_playerPrefab.name, _secondPosition.transform.position, Quaternion.identity);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+            PhotonNetwork.Instantiate(_playerPrefab.name, _firstPosition.transform.position, Quaternion.identity);
     }
 
     public void Exit()
@@ -49,22 +34,10 @@ public class GameManager : MonoBehaviourPunCallbacks, IPunObservable
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         Debug.Log("Player enter " + newPlayer.NickName);
-        Debug.Log("Player enter " + newPlayer.NickName + count);
     }
 
     public override void OnPlayerLeftRoom(Player otherPlayer)
     {
         Debug.Log("Player left " + otherPlayer.NickName);
-    }
-
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if(stream.IsWriting)
-        {
-            stream.SendNext(count);
-        } else
-        {
-            count = (int)stream.ReceiveNext();
-        }
     }
 }
