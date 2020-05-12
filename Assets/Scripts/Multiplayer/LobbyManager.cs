5cf4c3e5f41277aa3bd102850819eedd160232da
Максим <maks.greb2@gmail.com>
2020-05-12 12:24:23 +0300

    Поправил канвас

--- Assets/Scripts/Multiplayer/LobbyManager.cs
+++ Assets/Scripts/Multiplayer/LobbyManager.cs
@@ -6,12 +6,9 @@ using UnityEngine.UI;
 
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
-    public Text logText;
-
     void Start()
     {
         PhotonNetwork.NickName = "Player" + Random.Range(10, 99);
-        Log("Players name = " + PhotonNetwork.NickName);
         PhotonNetwork.AutomaticallySyncScene = true;
         PhotonNetwork.GameVersion = "1";
         PhotonNetwork.ConnectUsingSettings();
@@ -29,19 +26,6 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
     public override void OnJoinedRoom()
     {
-        Log("joined room");
         PhotonNetwork.LoadLevel("MultiplayerGame");
-    }
-
-    public override void OnConnectedToMaster()
-    {
-        Log("Connected to master");
-    }
-
-    public void Log(string message)
-    {
-        Debug.Log(message);
-        logText.text += "\n";
-        logText.text += message;
     }
 }
